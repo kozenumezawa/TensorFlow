5fd4f9a1e17682c585abe1c07800821c139a1c01
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-06-26 19:59:45 +0900

    Update sample cold

--- tutorial/tensorboard-sample.py
+++ tutorial/tensorboard-sample.py
@@ -1,53 +1,64 @@
+#Reference: http://qiita.com/sergeant-wizard/items/c98597b8add04b8eea0b
+#           http://qiita.com/sergeant-wizard/items/af7a3bd90e786ec982d2
+
 import tensorflow as tf
-import numpy as np
 
-WW = np.array([[0.1, 0.6, -0.9],
-               [0.2, 0.5, -0.8],
-               [0.3, 0.4, -0.7],
-               [0.4, 0.3, -0.6],
-               [0.5, 0.2, -0.5]]).astype(np.float32)
-bb = np.array([0.3, 0.4, 0.5]).astype(np.float32)
-x_data = np.random.rand(100,5).astype(np.float32)
-y_data = np.dot(x_data, WW) + bb
+input = [
+[1., 0., 0.],
+[0., 1., 0.],
+[0., 0., 1.]
+]
 
-with tf.Session() as sess:
+winning_hands = [
+[0., 1., 0.],
+[0., 0., 1.],
+[1., 0., 0.]
+]
 
-    W = tf.Variable(tf.random_uniform([5,3], -1.0, 1.0), name="W")
-    # The zeros set to zero with all elements.
-    b = tf.Variable(tf.zeros([3]), name="b")
-    #y = W * x_data + b
-    y = tf.matmul(x_data, W) + b
 
-    # Add summary ops to collect data
-    w_hist = tf.histogram_summary("weights", W)
-    b_hist = tf.histogram_summary("biases", b)
-    y_hist = tf.histogram_summary("y", y)
+def inference(input_placeholder):
+    with tf.name_scope('inference') as scope:
+        W = tf.Variable(tf.zeros([3, 3]), name="weight")
+        b = tf.Variable(tf.zeros([3]), name="bias")
+        y = tf.nn.softmax(tf.matmul(input_placeholder, W) + b)
+    return y
 
-    # Minimize the mean squared errors.
-    loss = tf.reduce_mean(tf.square(y - y_data))
-    # Outputs a Summary protocol buffer with scalar values
-    loss_summary = tf.scalar_summary("loss", loss)
+def loss(output, supervisor_labels_placeholder):
+    with tf.name_scope('loss') as scope:
+        cross_entropy = -tf.reduce_sum(supervisor_labels_placeholder * tf.log(output))
+        tf.scalar_summary("x-entropy", cross_entropy)
+    return cross_entropy
 
-    # Gradient descent algorithm
-    optimizer = tf.train.GradientDescentOptimizer(0.5)
-    train = optimizer.minimize(loss)
+def training(loss):
+    with tf.name_scope('training') as acope:
+        train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+    return train_step
 
-    # Before starting, initialize the variables.  We will 'run' this first.
+
+#   Variables are defined here to write variables in TensorBoard
+with tf.Graph().as_default():
+    supervisor_labels_placeholder = tf.placeholder("float", [None,3], name="supervisor_labels_placeholder")
+    input_placeholder = tf.placeholder("float", [None, 3], name="input_placeholder")
+
+    #   feed_dict denotes input of placeholder
+    feed_dict = {input_placeholder: input, supervisor_labels_placeholder: winning_hands}
+
+    output = inference(input_placeholder)
+    loss = loss(output, supervisor_labels_placeholder)
+    training_op = training(loss)
+
+    summary_op = tf.merge_all_summaries()
+
     init = tf.initialize_all_variables()
 
-    # Creates a SummaryWriter
-    # Merges all summaries collected in the default graph
-    merged = tf.merge_all_summaries()
-    writer = tf.train.SummaryWriter("./tensorflow_log", sess.graph_def)
-    sess.run(init)
+    with tf.Session() as sess:
+        #   Write placeholders in TensorBoard
+        summary_writer = tf.train.SummaryWriter('tensorflow_data', graph_def=sess.graph_def)
+        sess.run(init)
 
-    # Fit the line
-    for step in xrange(501):
-        if step % 10 == 0:
-            result = sess.run([merged, loss])
-            summary_str = result[0]
-            acc = result[1]
-            writer.add_summary(summary_str, step)
-            print"step = %s acc = %s W = %s b = %s" % (step, acc, sess.run(W), sess.run(b))
-        else:
-            sess.run(train)
+        for step in range(1000):
+            sess.run(training_op, feed_dict=feed_dict)
+            if step % 100 == 0:
+                print sess.run(loss, feed_dict=feed_dict)
+                summary_str = sess.run(summary_op, feed_dict=feed_dict)
+                summary_writer.add_summary(summary_str, step)
