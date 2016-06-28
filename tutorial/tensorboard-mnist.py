8098e3b68788257ba2f9a6bf7429185673972b6d
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-06-28 18:03:07 +0900

    Update

--- tutorial/tensorboard-mnist.py
+++ tutorial/tensorboard-mnist.py
@@ -41,7 +41,7 @@ x_image = tf.reshape(x, [-1, 28, 28, 1])
 
 
 #   Antivation: activation function in Relu(Rectified Linear Unit)
-with tf.name_scope ('reluuu'):
+with tf.name_scope ('activation1'):
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1) #   Pooling: reduce dimensions
 
@@ -49,8 +49,9 @@ with tf.name_scope ('reluuu'):
 W_conv2 = weight_variable([5, 5, 32, 64], "W_conv2")
 b_conv2 = bias_variable([64], "b_conv2")
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+with tf.name_scope ('activation2'):
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
 
 # Densely Connected Layer
 W_fc1 = weight_variable([7 * 7 * 64, 1024], "W_fc1")
@@ -60,8 +61,9 @@ h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 #   Dropout
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+with tf.name_scope ('Dropout'):
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 # Reedout Layer
 W_fc2 = weight_variable([1024, 10], "W_fc2")
@@ -69,15 +71,28 @@ b_fc2 = bias_variable([10], "b_fc2")
 y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 #   Train and Evaluate the Model
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ *
-                                              tf.log(y_conv), reduction_indices=[1]))
+def cross_entropy(y_, y_conv):
+    with tf.name_scope('error') as scope:
+        cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ *
+                                                      tf.log(y_conv), reduction_indices=[1]))
+        return cross_entropy
+
+cross_entropy = cross_entropy(y_, y_conv)
+
+def trainning(cross_entropy):
+    with tf.name_scope('trainning') as scope:
+        return tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+train_step = trainning(cross_entropy)
+
+def accuracy(y_conv, y_):
+    with tf.name_scope('accuracy') as scope:
+        correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        return accuracy
+
+accuracy = accuracy(y_conv, y_)
 
 tf.scalar_summary("x-entropy", cross_entropy)
-
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
 
 #   Write placeholders in TensorBoard
 summary_writer = tf.train.SummaryWriter('tensorboard-mnist_data', graph_def=sess.graph_def)
@@ -85,7 +100,7 @@ summary_op = tf.merge_all_summaries()
 
 sess.run(tf.initialize_all_variables())
 
-for i in range(2000):
+for i in range(300):
     batch = mnist.train.next_batch(50)
     if i % 100 == 0:
         train_accuracy = accuracy.eval(
